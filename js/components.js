b6d16fde46e042da8ccdcf7880446aa5ce2d820c
adrian zambrano <adrianzamstrong@gmail.com>
2022-07-20 18:00:27 -0400

    second commit

--- js/components.js
+++ js/components.js
@@ -2,7 +2,7 @@ Vue.component("card", {
   props: ["dato", "img", "p"],
   template: `
             <div class="card mx-2" style="width: 18rem;">
-            <img src="{{  img  }}" class="card-img-top" alt="pizzas-images">
+            <img :src="img" class="card-img-top h-50" alt="pizzas-images">
             <div class="card-body d-flex justify-content-between flex-column align-items-center">
             <h5 class="card-title">{{ dato }}</h5>
             <p class="card-text">{{  p  }}</p>
@@ -17,14 +17,14 @@ Vue.component("card", {
 var app = new Vue({
   el: "#app",
   data: {
-    heading: "heading 1",
+    heading: "Pizza Fast",
     productos: "Menu",
     pizza1: "Todas las carnes",
     pizza2: "4 quesos",
     pizza3: "Margarita",
     pizza4: "Anchoas",
-    img1: "../img/carnes.png",
     img2: "../img/margarita.webp",
+    img1: "../img/carnes.png",
     img3: "../img/quesos.jpg",
     img4: "../img/anchoas.jpg",
     descriptionCarnes:
@@ -32,6 +32,6 @@ var app = new Vue({
     descriptionQueso:
       "Con queso parmesano, queso provolone, queso de cabra, y queso mozarella esta pizza es altamente recomendada",
     descriptionMar: "Pizza de queso Mozzarella, jamón, tomate al oreganato y albahaca sumamente deliciosa",
-    descriptionAnch: "Pizza de anchoas y doble queso.",
+    descriptionAnch: "Pizza de anchoas, tocino y doble queso.",
   },
 });
