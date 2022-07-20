60f54be7beea9c2ff2a8bc408bc83a2f39e8d5bc
adrian zambrano <adrianzamstrong@gmail.com>
2022-07-20 00:16:07 -0400

    making components

--- js/components.js
+++ js/components.js
@@ -1,14 +1,37 @@
 Vue.component("card", {
-  template:
-    " <tr> <td>pizza hawuaiana</td><td>1</td><td>maiz,jamon</td><td> 8.999 </td><td>+  -</tr>",
+  props: ["dato", "img", "p"],
+  template: `
+            <div class="card mx-2" style="width: 18rem;">
+            <img src="{{  img  }}" class="card-img-top" alt="pizzas-images">
+            <div class="card-body d-flex justify-content-between flex-column align-items-center">
+            <h5 class="card-title">{{ dato }}</h5>
+            <p class="card-text">{{  p  }}</p>
+            <a href="#" class="btn btn-primary btn-sm w-50">Pedir</a>
+            </div>
+            </div>
+        `,
 });
 
 
 
 var app = new Vue({
-    el: "#app",
-    data: {
-        heading: 'heading 1',
-        productos: "lista"
-    }
-})
+  el: "#app",
+  data: {
+    heading: "heading 1",
+    productos: "Menu",
+    pizza1: "Todas las carnes",
+    pizza2: "4 quesos",
+    pizza3: "Margarita",
+    pizza4: "Anchoas",
+    img1: "../img/carnes.png",
+    img2: "../img/margarita.webp",
+    img3: "../img/quesos.jpg",
+    img4: "../img/anchoas.jpg",
+    descriptionCarnes:
+      "Rica pizza de pollo, carne de cerdo, carne de vacuno, tocino, jamon y pepperoni",
+    descriptionQueso:
+      "Con queso parmesano, queso provolone, queso de cabra, y queso mozarella esta pizza es altamente recomendada",
+    descriptionMar: "Pizza de queso Mozzarella, jamón, tomate al oreganato y albahaca sumamente deliciosa",
+    descriptionAnch: "Pizza de anchoas y doble queso.",
+  },
+});
